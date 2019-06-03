978d018370cc9b1bb34a141f1dc79e0ecc8a9523
정소영 <starsuzi@naver.com>
2019-06-03 15:55:37 +0900

    response as dummy json

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello');
+  res.json({"kusteau":"app"});
 });
 
 module.exports = router;
