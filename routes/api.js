6bdfb9a84b899b302607922492f8d38ccd3188ab
Sodaking <17884002+Sodaking@users.noreply.github.com>
2019-06-12 03:18:44 +0900

    store as kusto.json

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs")
 
-router.get('/', function(req, res, next) {
-  //res.send('Hello');
-  res.json({"kusteau":"app"});
-});
+var obj = {
+    kusto: []
+}
+
+router.get('/', function (req, res){
+    obj.kusto.push(req.query)
+    json = JSON.stringify(obj);
+    fs.writeFile( __dirname + "/../data/" + "kusto.json", json,'utf8', function (err, data){
+        res.json(obj);
+    });
+})
 
 module.exports = router;
