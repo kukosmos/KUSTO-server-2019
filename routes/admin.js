b49419fffc78b2e5aab54b0c329ba405294b30a0
Sodaking <17884002+Sodaking@users.noreply.github.com>
2019-07-21 23:04:06 +0900

    added multiple menu

--- routes/admin.js
+++ routes/admin.js
@@ -17,20 +17,33 @@ router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
 
 router.post('/write', function(req, res) {
+    var meta_path = __dirname + '/../data/' + req.body.category + '/meta.json';
+    var file = fs.readFileSync(meta_path, 'utf8');
+    var meta = JSON.parse(file);
+    
+    
+    if(meta[req.body.id] != 'undefined'){
+        console.log('id = ' + req.body.id + " already exist");
+        return;
+    }
+    
+    
     var obj = {};
-    var menu = req.body.menues;
-    var price = parseInt(req.body.price)
-    var menuArr = {
-       [menu] : price
-    };
+    var i = req.body.total_button;
+    var menuArr = {};
+    for(let j = 1; j <= i ; j++){
+        var menu = req.body["menu["+j+"]"];
+        var price = parseInt(req.body["price["+j+"]"]);
+        menuArr[menu]=price;
+    }
     var category = req.body.category;
     
     obj.id = req.body.id;
     obj.restaurant = req.body.restaurant;
     obj.location = req.body.location;
-    obj.rating = parseInt(req.body.rating);
-    obj.menues = menuArr;
-
+    obj.rating = parseFloat(req.body.rating);
+    obj.menues = menuArr;     
+    
     var json = JSON.stringify(obj, null, 2);
     fs.writeFile('data/'+category+'/'+req.body.id+'.json', json, (err)=> {
         if(err){
@@ -40,6 +53,26 @@ router.post('/write', function(req, res) {
             console.log('done');
         }
     });
+    
+    
+    var metaArr = {};
+    meta.count = meta.count +1;
+    metaArr['restaurant'] = req.body.restaurant;
+    metaArr['rating'] = parseFloat(req.body.rating);
+    meta[req.body.id] = metaArr;
+    
+    var data = JSON.stringify(meta, null, 2);
+    fs.writeFile(meta_path, data, (err)=> {
+        if(err){
+            console.error();
+        }
+        else{
+            console.log('done');
+        }
+    });
+    
+        
+    
   });
 
 module.exports = router;
